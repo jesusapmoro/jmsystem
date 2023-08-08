5d78ee3cb987cd74369ee44b69287225e55c88af
jesusapmoro <jesusapmoro@gmail.com>
2023-08-08 11:31:33 -0300

    Project created

---
+++ src/main/java/com/jesusapmoro/jmsystem/JmsystemApplication.java
@@ -0,0 +1,13 @@
+package com.jesusapmoro.jmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesusapmoro/jmsystem/JmsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusapmoro.jmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
