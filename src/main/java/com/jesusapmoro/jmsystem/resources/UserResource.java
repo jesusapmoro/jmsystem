06207828bb5a745283ca4711c12ee54db0d42e6b
jesusapmoro <jesusapmoro@gmail.com>
2023-08-08 13:37:25 -0300

    User entity and resource

---
+++ src/main/java/com/jesusapmoro/jmsystem/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.jesusapmoro.jmsystem.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jesusapmoro.jmsystem.entities.User;
+
+//essa clase é um recurso web que é implementado por um contolador leste. 
+//Colocar uma anotação em cima da classe
+//Dar um nome pro meu recurso, colocar aqui @RequestMapping
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	//Esse recurso é uses, criar um médoto que vai ser um end point para acessar os usúarios
+	//O tipo de retorno desse método vair ser um ResponseEntity que é um tipo específico do spring para retornar respostas de requisição Web
+	//Esse Response Entity é um tipo genéric ele espera um tipo, e o tipo da minha resposta vai ser a minha classe User.
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User (1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
+
