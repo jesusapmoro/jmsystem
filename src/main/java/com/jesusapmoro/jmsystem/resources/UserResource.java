d55ea352551cf953c04f0cdfe55e50c3a928bec9
jesusapmoro <jesusapmoro@gmail.com>
2023-08-08 15:54:55 -0300

    Service layer, component registration

--- src/main/java/com/jesusapmoro/jmsystem/resources/UserResource.java
+++ src/main/java/com/jesusapmoro/jmsystem/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.jesusapmoro.jmsystem.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jesusapmoro.jmsystem.entities.User;
+import com.jesusapmoro.jmsystem.services.UserService;
 
 //essa clase é um recurso web que é implementado por um contolador leste. 
 //Colocar uma anotação em cima da classe
@@ -14,15 +19,25 @@ import com.jesusapmoro.jmsystem.entities.User;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	//Esse recurso é uses, criar um médoto que vai ser um end point para acessar os usúarios
 	//O tipo de retorno desse método vair ser um ResponseEntity que é um tipo específico do spring para retornar respostas de requisição Web
 	//Esse Response Entity é um tipo genéric ele espera um tipo, e o tipo da minha resposta vai ser a minha classe User.
 	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User (1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
 
---
+++ src/main/java/com/jesusapmoro/jmsystem/services/UserService.java
@@ -0,0 +1,29 @@
+package com.jesusapmoro.jmsystem.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jesusapmoro.jmsystem.entities.User;
+import com.jesusapmoro.jmsystem.repositories.UserRepository;
+
+//registrar com dependência
+@Service
+public class UserService {
+	
+	//o meu UserService vai ter que ter uma dependência para o use repository
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
