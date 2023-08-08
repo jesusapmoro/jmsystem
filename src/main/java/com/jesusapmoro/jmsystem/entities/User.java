fb19237242346114321b568bf26a9f88c31a2691
jesusapmoro <jesusapmoro@gmail.com>
2023-08-08 14:06:51 -0300

    H2 database, test profile, JPA

--- src/main/java/com/jesusapmoro/jmsystem/entities/User.java
+++ src/main/java/com/jesusapmoro/jmsystem/entities/User.java
@@ -3,10 +3,22 @@ package com.jesusapmoro.jmsystem.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 //aqui no java existe uma interface chamada serializable, quando vc quer que seja transformado em cadeia de bytes, para trafegar em rede, seja salvo em arquivo
+//Colocar uma nnotation em cima do nome da classe chamada @Entity
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
